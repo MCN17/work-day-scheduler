ab9985cb9309daf79341709638d9cb723df2b5e7
Marty <martynolan17@gmail.com>
2022-06-27 14:50:34 -0400

    date is displayed at top of page

---
+++ Develop/script.js
@@ -0,0 +1,9 @@
+
+var currentTime = new Date();
+document.getElementById("currentDay").innerHTML = currentTime;
+
+
+// Another way to get the date to display at the top of the page
+// var currentTime = new Date();
+// displayCurrentTime = document.querySelector("#currentDay");
+// displayCurrentTime.textContent = currentTime;
