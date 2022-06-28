aedf0f1e5ee7aff702f8ca9f7c4b134456aa4684
Marty <martynolan17@gmail.com>
2022-06-28 10:47:06 -0400

    added function that changes time block color depending on current time

--- Develop/script.js
+++ Develop/script.js
@@ -8,3 +8,18 @@ document.getElementById("currentDay").innerHTML = currentTime;
 // displayCurrentTime = document.querySelector("#currentDay");
 // displayCurrentTime.textContent = currentTime;
 
+
+
+$("textarea").each(function () {
+    hour = moment().format("H");
+    var textArea = $(this);
+    var id = parseInt(textArea.attr("id"));
+
+    if (id < hour) {
+        $(this).addClass("past");
+    } else if (id > hour) {
+        $(this).addClass("future")
+    } else {
+        $(this).addClass("present")
+    }
+});
