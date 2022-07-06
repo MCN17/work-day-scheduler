342b6b4feabfb7aee72ea5f1d7a1fe0d3c26bd97
Marty <martynolan17@gmail.com>
2022-07-06 17:06:14 -0400

    moved folders around

--- assets/script.js
+++ assets/script.js
@@ -3,7 +3,6 @@
 var currentTime = moment().format("dddd, MM/DD/YY");
 document.getElementById("currentDay").innerHTML = currentTime;
 
-
 // Another way to get the date to display at the top of the page
 // var currentTime = new Date();
 // displayCurrentTime = document.querySelector("#currentDay");
